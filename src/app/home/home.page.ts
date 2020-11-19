0f222da7416bff8a441ef783de46ff4352e03982
Luis Felipe R <luisfelroca@hotmail.com>
2020-11-18 22:13:34 -0500

    cards y chips

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -41,7 +41,58 @@ export class HomePage implements OnInit {
       news: [
         {
           title: "Noticia 1",
-          summary: "Preview content",
+          summary: "Preview content noticia 1",
+          content: "Lorem ipsum",
+          image: "https://picsum.photos/200/300",
+          tags: [
+            {
+              id: 1,
+              name: "Fitness",
+            },
+            {
+              id: 2,
+              name: "Arroz con huevo",
+            },
+            {
+              id: 3,
+              name: "SmartFit",
+            },
+            {
+              id: 4,
+              name: "CS:GO",
+            },
+            {
+              id: 5,
+              name: "ABerica",
+            },
+          ],
+          imgSize: 3,
+          contentSize: 9,
+        },
+        {
+          title: "Noticia 2",
+          summary: "Preview content noticia 2",
+          content: "Lorem ipsum",
+          image: "https://picsum.photos/200/300",
+          tags: [
+            {
+              id: 1,
+              name: "Name 1",
+            },
+          ],
+          imgSize: 3,
+          contentSize: 9,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "Tecnologia",
+      icon: "appstore",
+      news: [
+        {
+          title: "Noticia 3",
+          summary: "Preview content noticia 3",
           content: "Lorem ipsum",
           image: "https://picsum.photos/200/300",
           tags: [
